3bc7c4d6487cb3d460ef432b1f83445cac777d1e
fahgundin <sfagundes.arthur@gmail.com>
2025-07-16 01:36:55 -0300

    relacionei o vite com o server

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,21 @@ import express from 'express'
 import publicRoutes from './routes/public.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 
 
 const app = express();
+const corsOptions = {
+    origin: ["http://127.0.0.1:5173"]
+}
+
+app.use(cors(corsOptions));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(express.static(__dirname));
 
 
 
@@ -21,15 +27,11 @@ app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.get('/',(req,res) =>{
 //    res.render(path.join(process.cwd(),'react', 'index'))
-    res.sendFile(path.join(__dirname, '../client/index.html'))
+    res.json({fruits: ["bananas", "macas"]})
 });
 
-
-
-
-
-
-
-app.listen(1000, () => console.log("Servidor Rodando"));
+app.listen('1000', () => {
+  console.log(`Servidor rodando em http://localhost:1000`);
+});
 
 //
--- client/react/src/App.jsx
+++ client/react/src/App.jsx
@@ -1,9 +1,22 @@
 import "./index.css";
 import Navbar from "./components/Navbar.jsx";
+import{useEffect} from "react";
+import axios from "axios"
 
 function App() {
+
+  const fetchAPI = async ()=>{
+    const response = await axios.get("http://localhost:1000/")
+    console.log(response.data.fruits)
+  }
+  useEffect(() =>{
+    fetchAPI();
+  }, [])
+
   return (
+    
     <div className="h-screen w-screen flex bg-slate-500">
+      
       <Navbar />
     </div>
   );
