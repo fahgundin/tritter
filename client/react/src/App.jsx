11ddf823ef8d95180fc6ffcb4fbb68f0740be2cb
Cauã Rodrigo <kauantv554@gmail.com>
2025-07-15 23:19:16 -0400

    tailwind e react confugrados

---
+++ client/react/src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import Navbar from "./components/Navbar.jsx";
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex bg-slate-500">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
