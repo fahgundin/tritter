6a6199c2a9851df5859b6b8c309df15cd993ddd0
Cauã Rodrigo <kauantv554@gmail.com>
2025-07-18 12:16:52 -0400

    Lucide e Router corrigidos

--- client/react/src/App.jsx
+++ client/react/src/App.jsx
@@ -1,8 +1,12 @@
 import "./index.css";
 import Navbar from "./components/Navbar.jsx";
-import{useEffect} from "react";
+import {useEffect} from "react";
 import axios from "axios"
-import Cadastro from "./components/Cadastro.jsx";
+import Cadastro from "./pages/Cadastro.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage.jsx";
+import Login from "./pages/Login.jsx";
+
 
 function App() {
 
@@ -15,14 +19,13 @@ function App() {
   }, [])
 
   return (
-    <div className="h-screen w-screen flex bg-slate-500">
-      <div>
-        <Navbar />
-      </div>
-      <div className="h-screen w-screen">
-        <Cadastro />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/react/src/components/Navbar.jsx
+++ client/react/src/components/Navbar.jsx
@@ -1,33 +1,42 @@
+import { Bell, Home, Mountain, PenLine, User } from "lucide-react"
+import { useNavigate } from "react-router-dom"
+
+
 function Navbar() {
+  const navigate = useNavigate();
+  const paraSignup = () => {
+    navigate("/signup");
+  };
+
   return (
     <header className="h-screen border-r-1 border-slate-400 pt-6">
       <div className="py-3 px-4 pl-5" alt="Pagina inicial">
-        <button className="text-left pl-4 w-32 h-10 hover:bg-slate-400 active:bg-slate-300 focus:outline-none focus:ring focus:ring-slate-200 rounded-full">
-          Página inicial
+        <button className="text-white text-left pl-4 w-[160px] h-10 hover:bg-slate-400 active:bg-slate-300 focus:outline-none focus:ring focus:ring-slate-200 rounded-full flex items-center">
+          <Home style={{ marginRight: "8px" }} /> Página inicial
         </button>
       </div>
 
       <div className="py-3 px-4 pl-5" alt="Notificações">
-        <button className="text-left pl-4 w-32 h-10 hover:bg-slate-400 active:bg-slate-300 focus:outline-none focus:ring focus:ring-slate-200 rounded-full">
-          Notificações
+        <button className="text-white text-left pl-4 w-[160px] h-10 hover:bg-slate-400 active:bg-slate-300 focus:outline-none focus:ring focus:ring-slate-200 rounded-full flex items-center">
+          <Bell style={{ marginRight: "8px" }} /> Notificações
         </button>
       </div>
 
       <div className="py-3 px-4 pl-5" alt="Explorar">
-        <button className="text-left pl-4 w-32 h-10 hover:bg-slate-400 active:bg-slate-300 focus:outline-none focus:ring focus:ring-slate-200 rounded-full">
-          Explorar
+        <button className="text-white text-left pl-4 w-[160px] h-10 hover:bg-slate-400 active:bg-slate-300 focus:outline-none focus:ring focus:ring-slate-200 rounded-full flex items-center">
+          <Mountain style={{ marginRight: '8px' }} /> Explorar
         </button>
       </div>
 
       <div className="py-3 px-4 pl-5" alt="Postar">
-        <button className="text-left pl-4 w-32 h-10 hover:bg-slate-400 active:bg-slate-300 focus:outline-none focus:ring focus:ring-slate-200 rounded-full">
-          Postar
+        <button className="text-white text-left pl-4 w-[160px] h-10 hover:bg-slate-400 active:bg-slate-300 focus:outline-none focus:ring focus:ring-slate-200 rounded-full flex items-center">
+          <PenLine style={{ marginRight: '8px' }} /> Postar
         </button>
       </div>
 
       <div className="py-3 px-4 pl-5" alt="Perfil">
-        <button className="text-left pl-4 w-32 h-10 hover:bg-slate-400 active:bg-slate-300 focus:outline-none focus:ring focus:ring-slate-200 rounded-full">
-          Perfil
+        <button onClick={paraSignup} className="text-white text-left pl-4 w-[160px] h-10 hover:bg-slate-400 active:bg-slate-300 focus:outline-none focus:ring focus:ring-slate-200 rounded-full flex items-center">
+          <User style={{ marginRight: '8px' }} /> Perfil
         </button>
       </div>
     </header>
