a1811d1ede52bda74d6b5c3d166a2a647a9a09a0
fahgundin <sfagundes.arthur@gmail.com>
2025-07-21 13:11:48 -0300

    tabela posts

--- backend/prisma/migrations/20250718175512_add_user_icon/migration.sql
+++ backend/prisma/migrations/20250718175512_add_user_icon/migration.sql
@@ -1,3 +1,11 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "biography" VARCHAR(255),
-ADD COLUMN     "user_icon" VARCHAR(255);
+
+CREATE TABLE "posts"(
+    "postID" SERIAL PRIMARY KEY not null,
+    "content" VARCHAR(455) not null, 
+    "userid" INTEGER NOT NULL,
+    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+
+    FOREIGN KEY (userid) REFERENCES users(userid)
+
+);
