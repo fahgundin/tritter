ce38b9b72d8c64fea05dd222107a76609a688233
fahgundin <sfagundes.arthur@gmail.com>
2025-07-29 14:45:36 -0300

    teste

--- client/react/vite.config.js
+++ client/react/vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+         host: '0.0.0.0', // Permite acesso de qualquer IP
+         // host: '192.168.1.100', // Substitua pelo seu IP
+         port: 3000,
+       },
 })
