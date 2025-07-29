4632e129e6e82e086f1321831ea5555ce55b893c
fahgundin <sfagundes.arthur@gmail.com>
2025-07-29 14:57:48 -0300

    teste

--- client/react/vite.config.js
+++ client/react/vite.config.js
@@ -4,10 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), sveltekit()],
   server: {
-         host: '0.0.0.0', // Permite acesso de qualquer IP
-         // host: '192.168.1.100', // Substitua pelo seu IP
+         host: '0.0.0.0', 
+         cors: {
+              origin: ['tritter.onrender.com', 'http://localhost:5173'],
+              methods: ['GET', 'POST','PUT', 'DELETE'],
+              allowedHeaders: ['Content-Type']
+		      },
+		    allowedHosts: ['mysubdomain.domain.io'],
+
          port: 3000,
        },
 })
